86929ef483f102cd1a4d31820a643a338a422570
Quasarkk <elderscry@gmail.com>
2024-01-07 21:16:47 +0100

    created models, seeders and controllers files
    created models, seeders and controllers files for :
    Pillar, Subpillar, Objective, Subobjective, Routine, Task

---
+++ app/Models/SubPillar.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SubPillar extends Model
+{
+    use HasFactory;
+}
