8f520fcd41f860a155580471b3a4a96475f171f8
Quasarkk <elderscry@gmail.com>
2024-01-12 19:27:11 +0100

    added a simple navbar to ease navigation during the development

---
+++ resources/js/Pages/Custom/RoutinePage.vue
@@ -0,0 +1,16 @@
+<template>
+    <AppLayoutVertical>
+        <h1>Routines</h1>
+    </AppLayoutVertical>
+</template>
+
+<script>
+import AppLayoutVertical from '@/Layouts/AppLayoutVertical.vue';
+
+export default {
+    components: {
+        draggable, AppLayoutVertical,
+    },
+}
+
+</script>
