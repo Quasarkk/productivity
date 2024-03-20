ace14606b217258c3b1c72cdd6a6e75f90186ca6
Quasarkk <elderscry@gmail.com>
2024-03-20 13:28:24 +0100

    basic landing page + fixed redirect on login

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -3,14 +3,14 @@
     <nav class="bg-gray-800 text-white p-4">
       <div class="container mx-auto flex justify-between items-center">
         <a href="/" class="text-lg font-semibold">Productivapp</a>
-        <div>
+        <div class="">
           <a href="/login" class="px-4 py-2 hover:bg-gray-700 rounded">Se connecter</a>
-          <a href="/register" class="px-4 py-2 hover:bg-gray-700 rounded">S'inscrire</a>
+          <a href="/register" class="bg-sky-600 hover:bg-sky-700 text-sky-100 rounded px-4 py-1 mx-auto text-xl ml-2">S'inscrire</a>
         </div>
       </div>
     </nav>
     <!-- Main Content -->
-    <main class="bg-red-100">
+    <main class="">
         <slot />
     </main>
     <!-- Footer -->
--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,12 @@
 <template>
-    <GuestLayout>
-        <h1 class="font-bold text-6xl text-center font-nunito">Finissez en avec la <br> <span class="text-green-800">procrastination</span></h1>
+    <GuestLayout class="font-nunito">
+        <h1 class="font-bold text-6xl text-center font-nunito">Finissez en avec la <br> <span class="">procrastination</span></h1>
+        <p class="text-center text-2xl">Progressez dans chacun des domaines qui comptes pour vous</p>
+        <div class="flex justify-center">
+            <button class="bg-sky-600 text-sky-100 rounded-lg px-2 py-1 mx-auto text-2xl">
+                S'inscrire
+            </button>
+        </div>
     </GuestLayout>
   </template>
 
